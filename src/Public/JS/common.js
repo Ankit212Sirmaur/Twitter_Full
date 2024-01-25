1594042bfa43ee505436de8efb2ea70baa2884fd
AnkitKumar <ankit17110747@yahoo.com>
2024-01-25 22:43:18 +0530

    ADDED js date to timestamp ago

--- src/Public/JS/common.js
+++ src/Public/JS/common.js
@@ -47,7 +47,7 @@ function createPostHtml(postData){
 
     let postedBy = postData.postedBy;
     const displayName = postedBy.firstName + " " + postedBy.lastName;
-    let timestamp = postData.createdAt;
+    let timestamp = timeDifference(new Date(), new Date(postData.createdAt))
     
     return `<div class='post'>
                 <div class = "mainContentContainer">
@@ -76,11 +76,47 @@ function createPostHtml(postData){
                             </div>
                             <div class = "postButtonContainer">
                                 <button>
-                                <i class = "fasb fa-retweet"></i>
+                                <i class = "fas fa-retweet"></i>
                                 </button>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>`
+}
+
+function timeDifference(current, previous) {
+
+    var msPerMinute = 60 * 1000;
+    var msPerHour = msPerMinute * 60;
+    var msPerDay = msPerHour * 24;
+    var msPerMonth = msPerDay * 30;
+    var msPerYear = msPerDay * 365;
+
+    var elapsed = current - previous;
+
+    if (elapsed < msPerMinute) {
+        if(elapsed/1000 < 30) return 'just now'; 
+         return Math.round(elapsed/1000) + ' seconds ago';   
+    }
+
+    else if (elapsed < msPerHour) {
+         return Math.round(elapsed/msPerMinute) + ' minutes ago';   
+    }
+
+    else if (elapsed < msPerDay ) {
+         return Math.round(elapsed/msPerHour ) + ' hours ago';   
+    }
+
+    else if (elapsed < msPerMonth) {
+        return Math.round(elapsed/msPerDay) + ' days ago';   
+    }
+
+    else if (elapsed < msPerYear) {
+        return Math.round(elapsed/msPerMonth) + ' months ago';   
+    }
+
+    else {
+        return Math.round(elapsed/msPerYear ) + ' years ago';   
+    }
 }
